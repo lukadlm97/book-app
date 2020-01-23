aa5a900deb1b7d148bfc7b096c037de4eab3de29
Luka Radovanovic <lukadlm97@gmail.com>
2020-01-23 02:12:59 +0100

    Created edit page, load data but not update table. Created Delete option and TempDate message

---
+++ BookListRazor/Pages/BookList/Edit.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookListRazor
+{
+    public class EditModel : PageModel
+    {
+        private readonly ApplicationDBContext _context;
+
+        public EditModel(ApplicationDBContext context)
+        {
+            _context = context; 
+        }
+
+        [BindProperty]
+        public Book Book { get; set; }
+        public async Task OnGet(int id)
+        {
+            Book = await _context.Books.FindAsync(id);
+
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                var BookFormDb = await _context.Books.FindAsync(Book.Id);
+               
+                BookFormDb.Name = Book.Name;
+                BookFormDb.ISBN = Book.ISBN;
+                BookFormDb.Author = Book.Author;
+
+                await _context.SaveChangesAsync();
+
+                return RedirectToPage("Index");
+            }
+            return RedirectToPage();
+        }
+    }
+}
--- BookListRazor/Pages/BookList/Index.cshtml.cs
+++ BookListRazor/Pages/BookList/Index.cshtml.cs
@@ -18,10 +18,26 @@ namespace BookListRazor
             _context = context;
         }
 
+        [TempData]
+        public string Message { get; set; }
+
         public IEnumerable<Book> Books { get; set; }
         public async Task OnGet()
         {
             Books = await _context.Books.ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPostDelete(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+            if (book==null)
+            {
+                return NotFound();
+            }
+            _context.Books.Remove(book);
+            await _context.SaveChangesAsync();
+            Message = "Book deleted successfully"; 
+            return RedirectToPage("Index");
         }
     }
 }
